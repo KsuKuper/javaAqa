bcc3c7dafd756b162e21ee17f39c6b79546ccd2d
x.kupriyanovich <nekrashevich.k@mail.ru>
2024-12-30 15:00:30 +0300

    Fixed comments

--- Hw1Tasks.java
+++ Hw1Tasks.java
@@ -1,18 +1,19 @@
-public class hw1Tasks {
+public class Hw1Tasks {
     public static void main(String[] args) {
         printNumWithIf();
         printNumWithSwitch();
         calculateEvenSum();
     }
+
     public static void printNumWithIf() {
-        for (int i=1; i<=5; i++) {
-            if(i==1) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 1) {
                 System.out.println("one");
-            } else if(i==2) {
+            } else if (i == 2) {
                 System.out.println("two");
-            } else if(i==3) {
+            } else if (i == 3) {
                 System.out.println("three");
-            } else if(i==4) {
+            } else if (i == 4) {
                 System.out.println("four");
             } else {
                 System.out.println("five");
@@ -21,34 +22,35 @@ public class hw1Tasks {
     }
 
     public static void printNumWithSwitch() {
-        for (int i=1; i<=5; i++) {
+        for (int i = 1; i <= 5; i++) {
             switch (i) {
                 case 1:
-                System.out.println("1");
-                break;
+                    System.out.println("1");
+                    break;
                 case 2:
-                System.out.println("2");
-                break;
+                    System.out.println("2");
+                    break;
                 case 3:
-                System.out.println("3");
-                break;
+                    System.out.println("3");
+                    break;
                 case 4:
-                System.out.println("4");
-                break;
+                    System.out.println("4");
+                    break;
                 case 5:
-                System.out.println("5");
-                break;
+                    System.out.println("5");
+                    break;
                 default:
                     break;
             }
         }
     }
+
     public static void calculateEvenSum() {
         int sum = 0;
         int i = 10;
-        while (i<=55) {
-            if (i%2==0){
-                sum+=i;
+        while (i <= 55) {
+            if (i % 2 == 0) {
+                sum += i;
             }
             i++;
         }
