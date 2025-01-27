d637dec4b2ff06d413d66d1a04420c94942ee611
x.kupriyanovich <nekrashevich.k@mail.ru>
2025-01-27 09:54:20 +0300

    Implemented methods: numberOfSunrise, sunrise and displayNumberOfSunrise.

---
+++ Hw4Tasks.java
@@ -0,0 +1,9 @@
+public class Hw4Tasks {
+    public static void main(String[] args) {
+        Sun sun = new Sun();
+        sun.sunrise();
+        sun.sunrise();
+        sun.sunrise();
+        sun.displayNumberOfSunrise();
+    }
+}
